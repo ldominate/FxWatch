268ebe41a391acc7a8e85d1c19c908105a7a4ec9
Евгений <loki_jh@bk.ru>
2017-08-06 22:53:27 +1000

    #32: Добавил отображение загруженных данных по периодам и фин. инструментам

--- modules/News/views/default/update.php
+++ modules/News/views/default/update.php
@@ -2,7 +2,9 @@
 
 use app\modules\catalog\models\FinTool;
 use app\modules\catalog\models\Period;
+use app\modules\news\models\NewsData;
 use yii\bootstrap\Html;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\news\models\News */
@@ -15,6 +17,17 @@ $this->params['breadcrumbs'][] = 'Обновить';
 $fintools = FinTool::find()->all();
 $periods = Period::find()->all();
 
+$exists_newsdata = NewsData::find()->where(['news_id' => $model->id])->select(['fintool_id', 'period_id'])->groupBy(['fintool_id', 'period_id'])->asArray()->all();
+$period_ids = array_unique(ArrayHelper::getColumn($exists_newsdata, 'fintool_id'));
+$exists_period = [];
+foreach ($period_ids as $p){
+	foreach ($exists_newsdata as $en){
+		if($en['fintool_id'] == $p){
+			$exists_period[$p][] = $en['period_id'];
+		}
+	}
+}
+
 ?>
 <div class="news-update">
 
@@ -22,20 +35,25 @@ $periods = Period::find()->all();
 
 	<div class="row">
 		<?php foreach ($fintools as $fintool): ?>
-			<div class="col-xs-8 col-sm-6 col-md-4 col-lg-3">
+			<div class="col-xs-8 col-sm-4 col-md-3 col-lg-2">
 				<div class="thumbnail">
 					<div class="caption">
 						<h4><?=$fintool->name?></h4>
 						<?php foreach ($periods as $period): ?>
 							<div class="row bottom-buffer">
-								<div class="col-xs-6 col-sm-7 col-md-6 col-lg-6 text-left"><?=$period->name?></div>
+								<div class="col-xs-6 col-sm-7 col-md-6 col-lg-6 text-left">
+									<?= Html::a(
+										$period->name,
+										\yii\helpers\Url::to(['/news/default/newsdata', 'news_id' => $model->id, 'fintool_id' => $fintool->id, 'period_id' => $period->id]),
+										['title' => 'Изменить данные'])?>
+									<?= (isset($exists_period[$fintool->id]) && ArrayHelper::isIn($period->id, $exists_period[$fintool->id]))
+										? Html::icon('glyphicon glyphicon-save', ['title' => 'Данные загружены'])
+										: ''?>
+								</div>
 								<div class="col-xs-6 col-sm-5 col-md-6 col-lg-6 text-right">
-									<?= Html::a(Html::icon(
-											'glyphicon glyphicon-pencil'),
-											\yii\helpers\Url::to(['/news/default/newsdata', 'news_id' => $model->id, 'fintool_id' => $fintool->id, 'period_id' => $period->id]),
-											['class' => 'btn btn-primary btn-xs', 'title' => 'Изменить'])?>
-									&nbsp;
-									<?= Html::a(Html::icon('glyphicon glyphicon-trash'), ['#'], ['class' => 'btn btn-warning btn-xs', 'title' => 'Удалить'])?>
+									<?= (isset($exists_period[$fintool->id]) && ArrayHelper::isIn($period->id, $exists_period[$fintool->id]))
+										? Html::a(Html::icon('glyphicon glyphicon-trash'), ['#'], ['class' => '', 'title' => 'Удалить'])
+										: '&nbsp;'?>
 								</div>
 							</div>
 						<?php endforeach; ?>
