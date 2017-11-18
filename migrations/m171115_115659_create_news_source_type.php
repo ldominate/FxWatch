2d943a2553fd7089f1173c47b20c1b1f164e4593
Евгений <loki_jh@bk.ru>
2017-11-18 12:53:55 +1000

    #129: Добавил миграцию создания таблицы типа инструмента

---
+++ migrations/m171115_115659_create_news_source_type.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\db\Migration;
+
+class m171115_115659_create_news_source_type extends Migration
+{
+    public function safeUp()
+    {
+		$this->createTable('sourcetype', [
+			'id' => $this->primaryKey(),
+			'type' => $this->integer(2)->unsigned()->notNull(),
+			'name' => $this->string(50)->notNull()->defaultValue('')
+		], 'CHARACTER SET utf8 DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB');
+
+		$this->batchInsert('sourcetype', ['type', 'name'],
+			[
+				[0, 'Валютные пары'],
+				[1, 'Финансовые инструменты']
+			]);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('sourcetype');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m171115_115659_create_news_source_type cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
